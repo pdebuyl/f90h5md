321010d824764f007b12fea49f899c32dbaf7d21
Pierre de Buyl <pdebuyl@nononospam.seriously.ulb.ac.be>
2011-03-23 12:15:31 -0400

    Added python generating script.

---
+++ gen_subr.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+
+types = dict()
+types['i'] = 'integer'
+types['d'] = 'double precision'
+H5T = dict()
+H5T['i'] = 'H5T_NATIVE_INTEGER'
+H5T['d'] = 'H5T_NATIVE_DOUBLE'
+dims = dict()
+dims['s'] = ''
+dims['1'] = '(:)'
+dims['2'] = '(:,:)'
+
+for t_k,t_v in types.iteritems():
+    for d_k,d_v in dims.iteritems():
+        if (d_k == 's'):
+            rank = 1
+        else:
+            rank = int(d_k)+1
+        s=''
+        s+="""
+  subroutine h5md_create_obs_%s%s(file_id, name, ID, data, link_from)
+    integer(HID_T), intent(inout) :: file_id
+    character(len=*), intent(in) :: name
+    type(h5md_t), intent(out) :: ID
+    %s, intent(in) :: data%s
+    character(len=*), intent(in), optional :: link_from
+
+    integer(HID_T) :: file_s, plist, g_id
+    integer(HSIZE_T), allocatable :: dims(:), max_dims(:), chunk_dims(:)
+    integer :: rank
+
+    call h5gcreate_f(file_id, 'observables/'//name, g_id, h5_error)
+
+    rank = %i
+    allocate(dims(rank)) ; allocate(max_dims(rank)) ; allocate(chunk_dims(rank))
+""" % (t_k,d_k, t_v, d_v, rank )
+
+        if (d_k!='s'):
+            s+="""
+    dims(1:%i) = shape(data)
+    max_dims(1:%i) = shape(data)
+    chunk_dims(1:%i) = shape(data)
+    
+""" % (rank-1, rank-1, rank-1)
+
+
+        s+="""
+    dims(%i)     = 0
+    max_dims(%i) = H5S_UNLIMITED_F
+    call h5screate_simple_f(rank, dims, file_s, h5_error, max_dims)
+    chunk_dims(%i) = 128
+    call h5pcreate_f(H5P_DATASET_CREATE_F, plist, h5_error)
+    call h5pset_chunk_f(plist, rank, chunk_dims, h5_error)
+    call h5dcreate_f(g_id, 'samples', %s, file_s, ID%% d_id, h5_error, plist)
+    call h5pclose_f(plist, h5_error)
+    call h5sclose_f(file_s, h5_error)
+
+    if (present(link_from)) then
+       call h5lcreate_hard_f(file_id, 'observables/'//link_from//'/step', g_id, 'step', h5_error)
+       call h5lcreate_hard_f(file_id, 'observables/'//link_from//'/time', g_id, 'time', h5_error)
+    else
+       call h5md_create_step_time(g_id)
+    end if
+
+    call h5dopen_f(g_id, 'step', ID%% s_id, h5_error)
+    call h5dopen_f(g_id, 'time', ID%% t_id, h5_error)
+
+    call h5gclose_f(g_id, h5_error)
+
+  end subroutine h5md_create_obs_%s%s
+
+""" % (rank,rank,rank, H5T[t_k],t_k,d_k)
+        print s
